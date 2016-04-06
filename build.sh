1624ac5ef8dbedf47955bda848c35a56ad26620d
jiang_huafeng <jiang_huafeng@technicolor.com>
2016-04-06 10:22:22 +0800

    boot from sdcard

--- build.sh
+++ build.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
-
-DESTDIR=/tmp/FriendlyARM/mini2440/kernel
-rm -rf ${DESTDIR}
-mkdir -p ${DESTDIR}
-
-touch .scmversion
-for suffix in a70 a70i h43 l80 n35 n43 p35 p43 s70 t35 td35 vga1024x768 vga640x480 vga800x600 w35 x35; do
-	cp config_mini2440_${suffix} .config && \
-		make -j6 && \
-		cp -vf arch/arm/boot/zImage ${DESTDIR}/zImage_${suffix^^} || exit 1
-done
-
-echo "done!"
-ls -l /tmp/FriendlyARM/mini2440/kernel/*
+export PATH=/work/workspace/sourcecode/2440/toolschain/4.4.3/bin:$PATH
+cp config_mini2440_n35 .config
+make uImage
+cp -r /home/jhf/sourcecode/2440/mini2440-linux-2.6.32.2/arch/arm/boot/uImage ./
